84d62664ae73055c9045a6dbc5e5d2c14086f817
James Beattie <jamesbeattie@James-MacBook-Pro.local>
2020-01-25 10:04:59 +1100

    Cleaning.

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
 
     # Directory names
-    dirNames   = ["M2MA0.1","M2MA0.5", "M2MA1", "M2MA10", "M2MA2",
+    dirNames   = ["M2MA1", "M2MA10", "M2MA2",
                   "M4MA0.1","M4MA0.5", "M4MA1", "M4MA10", "M4MA2",
                   "M10MA0.1","M10MA0.5", "M10MA1", "M10MA10", "M10MA2",
                   "M20MA0.1","M20MA0.5", "M20MA1", "M20MA10", "M20MA2"]
@@ -64,75 +64,80 @@ if __name__ == "__main__":
         # for each file
         for fileNumber in fileNumbers:
             fileCount += 1
-
-            print("Starting on file number: {} in directory: {}".format(fileNumber,dir))
-
-            # Load in the data
-            file3DDir   = readDir + dir +"/3DDens/"
-            fileProjDir = readDir + dir +"/Proj/"
             try:
-                # Calculate the 3D variance and delete the 3D field to save memory
-                print("Reading in 3D density.")
-                dens3D      = loadObj(file3DDir + "Turb_hdf5_plt_cnt_00{}_{}_dens".format(fileNumber,dir))
-                var3D       = (dens3D / dens3D.mean() - 1).var()
-                del dens3D
 
-                print("Reading in 2D projection.")
-                # Load the density projections and power spectra
-                densProj    = loadObj(fileProjDir + "Turb_hdf5_plt_cnt_00{}_{}_proj".format(fileNumber,dir))
-                varProj     = (densProj / densProj.mean() - 1).var()
-            except:
-                print("Skipping file number: {}".format(fileNumber))
+                print("Starting on file number: {} in directory: {}".format(fileNumber,dir))
 
-                # Check if we are at the end of the files (there are 100)
-                if fileNumber > 98:
-                    print("Saving object because you have reached then end of the files.")
-                    print("------------------------------------------------- \n")
-                    f.close()
-                    break
+                # Load in the data
+                file3DDir   = readDir + dir +"/3DDens/"
+                fileProjDir = readDir + dir +"/Proj/"
+                try:
+                    # Calculate the 3D variance and delete the 3D field to save memory
+                    print("Reading in 3D density.")
+                    dens3D      = loadObj(file3DDir + "Turb_hdf5_plt_cnt_00{}_{}_dens".format(fileNumber,dir))
+                    var3D       = (dens3D / dens3D.mean() - 1).var()
+                    del dens3D
 
-                continue
+                    print("Reading in 2D projection.")
+                    # Load the density projections and power spectra
+                    densProj    = loadObj(fileProjDir + "Turb_hdf5_plt_cnt_00{}_{}_proj".format(fileNumber,dir))
+                    varProj     = (densProj / densProj.mean() - 1).var()
+                except:
+                    print("Skipping file number: {}".format(fileNumber))
 
-            print("Calculating 2D power spectra.")
-            # Calculate the 2D power spectrum
-            densPS, k   = PSF.PowerSpectrum(densProj)
-            logDensPS   = np.log10(densPS)
+                    # Check if we are at the end of the files (there are 100)
+                    if fileNumber > 98:
+                        print("Saving object because you have reached then end of the files.")
+                        print("------------------------------------------------- \n")
+                        f.close()
+                        break
 
-            # Define some visualisation parameters
-            sigma       = 10                        # for Guassian kernel for contour plots
-            isobars     = np.array([0,-20,40])      # define the domain for the isobars on contour plots in log10 [to, from, by]
+                    continue
 
-            # Plot the column density and the power spectrum
-            if args['viz'] == True:
-                print("Creating a denisty and power spectrum plot.")
-                PF.kAndDensityFieldPlot(densProj,densPS,logDensPS,sigma,isobars,fileName)
+                print("Calculating 2D power spectra.")
+                # Calculate the 2D power spectrum
+                densPS, k   = PSF.PowerSpectrum(densProj)
+                logDensPS   = np.log10(densPS)
 
-            print("Fitting ellipses.")
-            # Fit and Calculate the ellipses to all of the k-space power spectra
-            aniso, anisoStd, prinAxisKeep, center, kperpAxis, kparAxis  = EF.calculateEllipses(logDensPS,sigma,isobars,"kperp")
+                # Define some visualisation parameters
+                sigma       = 10                        # for Guassian kernel for contour plots
+                isobars     = np.array([0,-20,40])      # define the domain for the isobars on contour plots in log10 [to, from, by]
 
-            print("Reconstructing anisotropic dispersio.n")
-            # Calculate the anisotropic variances for the prolate and oblate ellipse.
-            varProlate, varOblate,  rr, cc, relError, error, aniso      = AR.calculateAnisoVar(densPS,center,aniso,viz=args['viz'])
+                # Plot the column density and the power spectrum
+                if args['viz'] == True:
+                    print("Creating a denisty and power spectrum plot.")
+                    PF.kAndDensityFieldPlot(densProj,densPS,logDensPS,sigma,isobars,fileName)
 
-            print("Reconstructing isotropic dispersion.")
-            # Calculate the isotropic variance for comparison.
-            R, var3DIso                                                 = PSF.calculateIsoVar(densPS,k,varProj)
+                print("Fitting ellipses.")
+                # Fit and Calculate the ellipses to all of the k-space power spectra
+                aniso, anisoStd, prinAxisKeep, center, kperpAxis, kparAxis  = EF.calculateEllipses(logDensPS,sigma,isobars,"kperp")
 
-            print("------------------------------------------------- \n")
+                print("Reconstructing anisotropic dispersio.n")
+                # Calculate the anisotropic variances for the prolate and oblate ellipse.
+                varProlate, varOblate,  rr, cc, relError, error, aniso      = AR.calculateAnisoVar(densPS,center,aniso,viz=args['viz'])
 
-            # Print off the values for the reconstructions
-            if args['print'] == True:
-                print("Relative residual power: {}".format(relError))
-                print("Absolute residual power correction: {}".format(error))
-                print("The 2D projection variance is: {}".format(varProj))
-                print("The 3D variance is: {}".format(var3D))
-                print("The variance from the 3D prolate: {}".format(varProlate))
-                print("The variance from the 3D oblate: {}".format(varOblate))
-                print("The variance from the isotropic recon: {}".format(var3DIso))
+                print("Reconstructing isotropic dispersion.")
+                # Calculate the isotropic variance for comparison.
+                R, var3DIso                                                 = PSF.calculateIsoVar(densPS,k,varProj)
+
                 print("------------------------------------------------- \n")
 
-            if fileCount == 1:
-                f.write("directory, fileNumber, relError, error, varProj, var3D, varProlate, varOblate, var3DIso \n".format(fileNumber))
+                # Print off the values for the reconstructions
+                if args['print'] == True:
+                    print("Relative residual power: {}".format(relError))
+                    print("Absolute residual power correction: {}".format(error))
+                    print("The 2D projection variance is: {}".format(varProj))
+                    print("The 3D variance is: {}".format(var3D))
+                    print("The variance from the 3D prolate: {}".format(varProlate))
+                    print("The variance from the 3D oblate: {}".format(varOblate))
+                    print("The variance from the isotropic recon: {}".format(var3DIso))
+                    print("------------------------------------------------- \n")
 
-            f.write("{} {}, {}, {}, {}, {}, {}, {}, {} \n".format(dir,fileNumber,relError,error,varProj,var3D,varProlate,varOblate,var3DIso))
+                if fileCount == 1:
+                    f.write("directory, fileNumber, relError, error, varProj, var3D, varProlate, varOblate, var3DIso \n".format(fileNumber))
+
+                f.write("{} {}, {}, {}, {}, {}, {}, {}, {} \n".format(dir,fileNumber,relError,error,varProj,var3D,varProlate,varOblate,var3DIso))
+            except:
+                print("Skipping number.")
+                print("------------------------------------------------- \n")
+                continue
---
+++ plotData.p
@@ -0,0 +1,54 @@
+# Plotting file
+####################################################################################
+
+# New plot
+reset
+
+# Set the out file
+
+
+# General theme (modified from Christoph)
+set term post eps color enhanced dashed dashlength 3.3 linewidth 1.5 "Times-Roman" 25
+unset title
+set key spacing 1.5 samplen 3
+set xlabel "t / T"
+set ylabel "sigma^2"
+set tics scale 1.3
+set mxtics 10
+set mytics 10
+unset log y
+unset log x
+set size 1.25, 1.25
+set origin 0.0, 0.0
+
+outputDat = "output_M4MA10.dat"
+
+set out "M4MA10.eps"
+
+p outputDat u ($2/10):6 with lines linestyle 1 lw 2 lt 1 title 'True dispersion', \
+  outputDat u ($2/10):7 with lines linestyle 1 lw 2 lt 2 title 'Prolate Recon.', \
+  outputDat u ($2/10):8 with lines linestyle 1 lw 2 lt 3 title 'Oblate Recon.',
+
+# New plot
+reset
+
+# Set the out file
+set out "M4MA10_cor.eps"
+
+# General theme (modified from Christoph)
+set term post eps color enhanced dashed dashlength 3.3 linewidth 1.5 "Times-Roman" 25
+unset title
+set key spacing 1.5 samplen 3
+set xlabel "sigma^2_{true}"
+set ylabel "sigma^2_{reconstructed}"
+set tics scale 1.3
+set mxtics 10
+set mytics 10
+unset log y
+unset log x
+set size 1.25, 1.25
+set origin 0.0, 0.0
+
+p outputDat u 6:7 lw 2 lt 1 title 'True dispersion', \
+  outputDat u 6:8 lw 2 lt 2 title 'Prolate Recon.', \
+  x with lines lw 2 lt -1
